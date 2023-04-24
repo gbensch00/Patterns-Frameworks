c3f0adb6dd5323edc4ca57280689debcf70923de
Tobias <anemone.verlauf0v@icloud.com>
2023-04-24 11:45:03 +0200

    Integration Login-Screen und Account Erstellungs Screen

--- Main.java
+++ Main.java
@@ -1,21 +1,35 @@
+import java.util.ArrayList;
+
 import Controller.GameController;
 import Model.GameModel;
 import View.GameView;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-  public static void main(String[] args) {
-    launch(args);
-  }
+	private ArrayList<ImageView> bullets;
 
-  @Override
-  public void start(Stage primaryStage) throws Exception {
-    GameModel model = new GameModel();
-    GameView view = new GameView();
-    GameController controller = new GameController(model, view);
+	public static void main(String[] args) {
+		launch(args);
+	}
 
-    primaryStage.setScene(view.getScene());
-    primaryStage.show();
-  }
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+
+		Parent root = FXMLLoader.load(getClass().getResource("/Controller/main.fxml"));
+		Scene scene = new Scene(root);
+		
+	/*Für spätere CSS-Anpassungen oder Intergration*/
+		
+		// scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		
+		primaryStage.setScene(scene);
+		primaryStage.setResizable(false);
+		primaryStage.show();
+
+	}
 }
